14563e64c2afb9bb3b85b4c67756394eea99acca
MingTAT <314725486@qq.com>
2016-07-29 15:02:14 +0800

    0729	0729

---
+++ skeleton/src/hw3/CatchAllEvaluator.java
@@ -0,0 +1,27 @@
+package hw3;
+
+/**
+ * Evaluator satisfied by any set of cards.  The number of
+ * required cards is equal to the hand size.
+ * 
+ * The name of this evaluator is "Catch All".
+ */
+//Note: You must edit this declaration to extend AbstractEvaluator
+//or to extend some other class that extends AbstractEvaluator
+public class CatchAllEvaluator
+{
+  /**
+   * Constructs the evaluator.
+   * @param ranking
+   *   ranking of this hand
+   * @param handSize
+   *   number of cards in a hand
+   */
+  public CatchAllEvaluator(int ranking, int handSize)
+  {
+    // TODO: call appropriate superclass constructor and 
+    // perform other initialization
+  }
+  
+
+}
---
+++ skeleton/src/hw3/FullHouseEvaluator.java
@@ -0,0 +1,33 @@
+package hw3;
+
+/**
+ * Evaluator for a generalized full house.  The number of required
+ * cards is equal to the hand size.  If the hand size is an odd number
+ * n, then there must be (n / 2) + 1 cards of the matching rank and the
+ * remaining (n / 2) cards must be of matching rank. In this case, when constructing
+ * a hand, the larger group must be listed first even if of lower rank
+ * than the smaller group</strong> (e.g. as [3 3 3 5 5] rather than [5 5 3 3 3]).
+ * If the hand size is even, then half the cards must be of matching 
+ * rank and the remaining half of matching rank.  Any group of cards,
+ * all of which are the same rank, always satisfies this
+ * evaluator.
+ * 
+ * The name of this evaluator is "Full House".
+ */
+//Note: You must edit this declaration to extend AbstractEvaluator
+//or to extend some other class that extends AbstractEvaluator
+public class FullHouseEvaluator
+{
+  /**
+   * Constructs the evaluator.
+   * @param ranking
+   *   ranking of this hand
+   * @param handSize
+   *   number of cards in a hand
+   */
+  public FullHouseEvaluator(int ranking, int handSize)
+  {
+    // TODO: call appropriate superclass constructor and 
+    // perform other initialization
+  }
+}
